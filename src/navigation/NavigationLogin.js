6d8ee48a72842d44bda6bdbb7facb7e6d2ae75c0
AndresJR3 <andyjaimerodriguez@gmail.com>
2025-06-13 22:04:25 -0600

    revisen codigo

---
+++ src/navigation/NavigationLogin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+const Stack = createNativeStackNavigator();
+
+export default function NavigationLogin() {
+
+    <Stack.Navigator initialRouteName='NavigationLogin'>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+    </Stack.Navigator>
+}
